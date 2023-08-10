de15cd2af1d0b8e67385964ddb875ec3f12f66c9
wwl-jky <jky@winwinlab.co.kr>
2023-08-10 18:27:21 +0900

    [feature] 퍼블리싱

--- gwangmyeong/src/App.js
+++ gwangmyeong/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Header from './component/header';
 import Footer from './component/footer';
+import Gwangmyeong from './page/gwangmyeong';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+
+      <Gwangmyeong/>
 
       <Footer/>
     </div>
